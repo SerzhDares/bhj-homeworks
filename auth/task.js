df5ece99ed16d4ce9afe9c053fc7b61ef51e7ee0
SerzhDares <126406118+SerzhDares@users.noreply.github.com>
2023-09-19 20:36:08 +0400

    corrected

--- auth/task.js
+++ auth/task.js
@@ -9,18 +9,17 @@ form.addEventListener('submit', e => {
     e.preventDefault();
     const xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
+    xhr.responseType = 'json';
     const formData = new FormData(form);
     xhr.send(formData);
-    xhr.addEventListener('readystatechange', () => {
-        if(xhr.readyState === xhr.DONE) {
-            let response = JSON.parse(xhr.responseText);
-            if(response.success == true) {
-                localStorage.setItem('user_id', response.user_id);
-                spanId.textContent = localStorage.getItem('user_id');
-                welcome.classList.add('welcome_active');
-            } else {
-                alert('Неверный логин/пароль');
-            }
+    xhr.addEventListener('load', () => {
+        if(xhr.response.success == true) {
+            localStorage.setItem('user_id', xhr.response.user_id);
+            spanId.textContent = localStorage.getItem('user_id');
+            welcome.classList.add('welcome_active');
+        } else {
+            alert('Неверный логин/пароль');
         }
+        form.reset();
     })
 })
